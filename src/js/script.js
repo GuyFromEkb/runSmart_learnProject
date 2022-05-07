b945911c9bfee82ee6bff2bb9157cd1ed1736640
Sergey_nasty <zaebalacs@gmail.com>
2022-05-07 19:03:20 +0500

    сделал ещё 1 сектор,опробовал функцию слайдов через slick

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(document).ready(function() {
+    $('.carusel__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./icons/right.svg"></button>',
+        speed: 1200
+
+    });
+});

24085ddc1137fd617bcd5c785e7332c930ae5e94
Sergey_nasty <zaebalacs@gmail.com>
2022-05-09 07:58:52 +0500

    пробовал раззные вариации слайдеров кроме slicА 'owl carusel - чет мне не зашло, tiny-slider-норм' начал новую секцию начал изучать табы на сайте и подмену контента в блоке, через блок 200% и overflow: hidden

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,28 @@
 "use strict";
 
-$(document).ready(function() {
-    $('.carusel__inner').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./icons/right.svg"></button>',
-        speed: 1200
+var slider = tns({
+    container: '.carusel__inner',
+    items: 1,
+    speed: 1400,
+    loop: true,
+    rewind: true,
+    nav: false,
+    controls: false
 
-    });
 });
+
+document.querySelector('.carusel__prev-btn').onclick = function() {
+    slider.goTo('prev');
+};
+document.querySelector('.carusel__next-btn').onclick = function() {
+    slider.goTo('next');
+};
+
+// $(document).ready(function() {
+//     $('.carusel__inner').slick({
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//         speed: 1200
+
+//     });
+// });

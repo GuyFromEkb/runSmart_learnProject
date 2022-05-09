1022cfbb4a12fc73b915648ebcaf12c53ad9351f
Sergey_nasty <zaebalacs@gmail.com>
2022-05-09 20:05:37 +0500

    опробовал переключения табов через jquery + скопировал скрипт для таба в контейнере

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,30 @@ document.querySelector('.carusel__next-btn').onclick = function() {
     slider.goTo('next');
 };
 
+$(document).ready(function() {
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function TabInfo(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__content-info').eq(i).toggleClass('catalog-item__content-info_active');
+            });
+
+        });
+    }
+
+    TabInfo('.catalog-item__link');
+    TabInfo('.catalog-item__link_back');
+
+});
+
 // $(document).ready(function() {
 //     $('.carusel__inner').slick({
 //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
